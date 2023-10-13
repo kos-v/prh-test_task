7fd84abc61a25bc0d599a3706ca4cec382b4925a
Konstantin Voloshchuk <kosv.dev@gmail.com>
2023-10-14 02:22:24 +0600

    [feat] Init yii2 advanced template

---
+++ app/environments/prod/common/config/main-local.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'components' => [
+        'db' => [
+            'class' => \yii\db\Connection::class,
+            'dsn' => 'mysql:host=localhost;dbname=yii2advanced',
+            'username' => 'root',
+            'password' => '',
+            'charset' => 'utf8mb4',
+        ],
+        'mailer' => [
+            'class' => \yii\symfonymailer\Mailer::class,
+            'viewPath' => '@common/mail',
+        ],
+    ],
+];
