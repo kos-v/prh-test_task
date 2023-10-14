da612b398b05d3c635ce2d68cfdee870b2e963f0
Konstantin Voloshchuk <kosv.dev@gmail.com>
2023-10-14 22:06:32 +0600

    [lint] Integrate PHP CS Fixer

---
+++ app/.php-cs-fixer.php
@@ -0,0 +1,33 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->in(__DIR__ . '/backend')
+    ->in(__DIR__ . '/common')
+    ->in(__DIR__ . '/console')
+    ->in(__DIR__ . '/environments')
+    ->in(__DIR__ . '/frontend')
+    ->exclude('config')
+    ->exclude('mail')
+    ->exclude('runtime')
+    ->exclude('views')
+    ->exclude('web');
+
+
+$config = new PhpCsFixer\Config();
+return $config->setRules([
+    '@PSR12' => true,
+    'strict_param' => true,
+    'declare_strict_types' => true,
+    'no_unused_imports' => true,
+    'ordered_imports' => [
+        'sort_algorithm' => 'alpha',
+        'imports_order' => ['class', 'function', 'const'],
+    ],
+    'static_lambda' => true,
+    'global_namespace_import' => [
+        'import_classes' => true,
+        'import_constants' => true,
+        'import_functions' => true,
+    ],
+])
+    ->setFinder($finder);
