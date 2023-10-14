86c94b0cd92f5ecf0a17d5028bc8e096b50d97cc
Konstantin Voloshchuk <kosv.dev@gmail.com>
2023-10-14 23:07:54 +0600

    [feat] Add default user to UserFixture

--- app/common/fixtures/UserFixture.php
+++ app/common/fixtures/UserFixture.php
@@ -4,9 +4,26 @@ declare(strict_types=1);
 
 namespace common\fixtures;
 
+use common\models\User;
+use Yii;
 use yii\test\ActiveFixture;
 
 class UserFixture extends ActiveFixture
 {
-    public $modelClass = 'common\models\User';
+    public $modelClass = User::class;
+
+    protected function getData(): array
+    {
+        return [
+            'admin' => [
+                'username' => 'admin',
+                'email' => 'admin@test-task.local',
+                'auth_key' => Yii::$app->security->generateRandomString(),
+                'password_hash' => Yii::$app->security->generatePasswordHash('admin'),
+                'status' => User::STATUS_ACTIVE,
+                'created_at' => 1697303024,
+                'updated_at' => 1697303024,
+            ],
+        ];
+    }
 }
