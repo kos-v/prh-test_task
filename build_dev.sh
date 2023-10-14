c1baa2353c11767a94055bc68dedb16015b40a45
Konstantin Voloshchuk <kosv.dev@gmail.com>
2023-10-14 23:15:20 +0600

    [deployment] Add fixtures upload with default data to build script

--- build_dev.sh
+++ build_dev.sh
@@ -5,3 +5,4 @@ docker-compose up --build -d
 docker-compose exec app sh -c "composer install -d /project/app/ --no-interaction"
 docker-compose exec app sh -c "cd /project/app && ./init --env=Development --overwrite=All --delete=All"
 docker-compose exec app sh -c "cd /project/app && ./yii migrate --interactive=0"
+docker-compose exec app sh -c "cd /project/app && ./yii fixture '*' --namespace='common\fixtures' --interactive=0"
