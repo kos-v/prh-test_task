8cedb85d040da86c910de9fa8b65b204eae15dd9
Konstantin Voloshchuk <kosv.dev@gmail.com>
2023-10-16 02:20:39 +0600

    [feat] Implement AppleDnaService

---
+++ app/backend/services/AppleDnaService.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace backend\services;
+
+use common\models\Apple;
+
+use function count;
+use function mt_rand;
+
+class AppleDnaService
+{
+    private const ALLOWED_COLORS = ['#5cb85c', '#d9534f', '#eef04e'];
+
+    public function programGenes(Apple $apple): Apple
+    {
+        $apple->color = self::ALLOWED_COLORS[mt_rand(0, count(self::ALLOWED_COLORS) - 1)];
+        return $apple;
+    }
+}
